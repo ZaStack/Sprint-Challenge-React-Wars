5d477d60f7f4a2980c7e6771a8ee38dadea921d0
Zach Stack <stackz.personal@gmail.com>
2020-01-24 16:41:53 -0800

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,15 +6,14 @@ import './App.css';
 
 const AppStyle = styled.div `
   background: black;
-  width: 60%;
-  max-width: 1000px;
+  // width: 60%;
+  max-width: 75%;
   margin: 0 auto;
-  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  opacity: 0.6;
+  opacity: 0.7;
   -webkit-box-shadow: 5px 5px 15px 5px #000000; 
   box-shadow: 5px 5px 15px 5px #000000;
 
@@ -22,7 +21,7 @@ const AppStyle = styled.div `
 const HeaderStyle = styled.h1 `
   font-family: 'Pathway Gothic One', sans-serif;
   font-size: 400%;
-  font-weight: 600;
+  font-weight: 800;
   letter-spacing: 6px;
   line-height: 150%;
   margin-top: 5vh;
@@ -32,57 +31,30 @@ const HeaderStyle = styled.h1 `
 `
 const uuidv4 = require('uuid/v4');
 
-
 const App = () => {
-  const [starCharacter, setStarCharacter] = useState([]);
-  useEffect(() => {
-    axios 
-      .get('https://swapi.co/api/people/')
-      .then(response => {
-        // console.log("Initial data", response)
-        // setTimeout(function(){ 
-        //   alert("Hold On");         
-          setStarCharacter(response.data.results)
-        // }, 5000);
-        // console.log("Star data", starCharacter)
+  const uuidv4 = require("uuid/v4");
+  const [characters, setCharacters] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get(`https://swapi.co/api/people/`)
+      .then(response => {
+        console.log(response.data.results);
+        setCharacters(response.data.results);
       })
       .catch(error => {
-        console.log("the data was not returned", error)
-      })
-
+        console.log("Data did not return", error);
+      });
   }, []);
-
-  //   const starCharacter= {
-  //   name: "Luke Skywalker",
-  //   height: "172",
-  //   mass: "77",
-  //   hair_color: "blond",
-  //   skin_color: "fair",
-  //   eye_color: "blue",
-  //   birth_year: "19BB"
-  // }
-
-
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
 
   return (
     <AppStyle key={uuidv4()}>
-      <HeaderStyle>React Wars</HeaderStyle>
-      {starCharacter.map(p => (
-        <Card key={uuidv4()} character={starCharacter}/>
-      ))
-    }
+      <HeaderStyle key={uuidv4()}>React Wars</HeaderStyle>
+      {characters.map(character => {
+        return <Card key={uuidv4()} character={character} />;
+      })}
     </AppStyle>
-  )
-}
+  );
+};
 
-export default App
-
-
-// name={starCharacter.name} height={starCharacter.height} mass={starCharacter.mass} hair_color={starCharacter.hair_color} skin_color={starCharacter.skin_color} eye_color={starCharacter.eye_color} birth_year={starCharacter.birth_year}
+export default App;
--- starwars/src/components/Cards.js
+++ starwars/src/components/Cards.js
@@ -3,29 +3,46 @@ import styled from 'styled-components';
 
 const CardStyle = styled.div`
     margin: 3%;
-    width: 60%;
+    width: 80%;
     opacity: 0.7;
     border-radius: 10px;
-    padding: 2%;
+    // padding: 1%;
     color: #feda4a;
-    justify-self: flex-start;
-    transform: perspective(300px) rotateX(35deg);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    transform: perspective(300px) rotateX(15deg);
     line-height: 2;
-
+    font-size: 2rem;
+`
+const NameStyle = styled.header `
+    font-size: 3rem;
+    // max-width: 100%;
+    text-align: center;
+`
+const SpanStyle = styled.span `
+    font-family: 'Pathway Gothic One', sans-serif;
+    letter-spacing: 6px;
+    -webkit-text-fill-color: rgba(0, 0, 0, 0.5);
+    -webkit-text-stroke: 1px #feda4a;
+    font-weight: 400;
 
 `
-
+const PStyle = styled.p `
+    max-width: 100%;
+    text-align: left;
+`
 const Card = ({character}) => {
-    // console.log("Card props", props)
     return (
         <CardStyle>
-            <header><span>Name:</span> {character.name}</header>
-            <p><span>Height:</span> {character.species}</p>
-            {/* <p><span>Mass:</span> {character.mass}</p>
-            <p><span>Hair Color:</span> {character.hair_color}</p>
-            <p><span>Skin Color:</span> {character.skin_color}</p>
-            <p><span>Eye Color:</span> {character.eye_color}</p>
-            <p><span>Birth Year:</span> {character.birth_year}</p> */}
+            <NameStyle><SpanStyle>Name: </SpanStyle> {character.name}</NameStyle>
+            <PStyle><SpanStyle>Height: </SpanStyle> { character.height}</PStyle>
+            <PStyle><SpanStyle>Mass: </SpanStyle>{ character.mass}</PStyle>
+            <PStyle><SpanStyle>Hair Color: </SpanStyle> { character.hair_color}</PStyle>
+            <PStyle><SpanStyle>Skin Color: </SpanStyle> { character.skin_color}</PStyle>
+            <PStyle><SpanStyle>Eye Color: </SpanStyle>{ character.eye_color}</PStyle>
+            <PStyle><SpanStyle>Birth Year: </SpanStyle> { character.birth_year}</PStyle>
         </CardStyle>
     )
 }
